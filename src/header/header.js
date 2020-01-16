fb92925e31769c8d7ec69661a13db1bfd7c2b979
Alexander Varnaev <alexandervarnaev@gmail.com>
2020-01-17 00:26:36 +0300

    playing with state and clickHandler

--- src/header/header.js
+++ src/header/header.js
@@ -3,17 +3,12 @@ import Logo from './logo/logo';
 import './header.css'
 import Menu from './menu/menu'
 
-const menuItems = {
-    login: "Логин",
-    map: "Карта",
-    profile: "Профиль",
-    auth: "Войти",
-}
+
 const Header = () => {
     return (
         <div className="header">
             <Logo />
-            <Menu props={menuItems} />
+            <Menu />
         </div>
     )
 };
--- src/header/menu/menu.js
+++ src/header/menu/menu.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import MenuList from '../menu-list/menu-list';
 import './menu.scss'
-let handleClick = function () {
-    console.log('action!');
-}
 
-const Menu = (props) => {
+const Menu = () => {
     return (
         <nav className="menu">
-            <ul className="menu__list">
-                <li onClick={handleClick} className="menu__item">{props.props.login}</li>
-                <li onClick={handleClick} className="menu__item">{props.props.map}</li>
-                <li onClick={handleClick} className="menu__item">{props.props.profile}</li>
-                <li onClick={handleClick} className="menu__item">{props.props.auth}</li>
-            </ul>
+            <MenuList />
         </nav>
     )
 };
